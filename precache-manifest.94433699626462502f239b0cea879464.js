24fc8828fb3cb9ff43773466480430d6bc8a38ad
VH <vhuy371@gmail.com>
2020-10-08 10:56:31 +1100

    Updates

---
+++ precache-manifest.94433699626462502f239b0cea879464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415a1ff5b1b3102ebdf6e4f520bb16e1",
+    "url": "/tax-calculator/index.html"
+  },
+  {
+    "revision": "de32a5c54b2a0f78d3b0",
+    "url": "/tax-calculator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a878905698bd854e7f65",
+    "url": "/tax-calculator/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "de32a5c54b2a0f78d3b0",
+    "url": "/tax-calculator/static/js/2.3fe3d4dd.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/tax-calculator/static/js/2.3fe3d4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a878905698bd854e7f65",
+    "url": "/tax-calculator/static/js/main.d76451e4.chunk.js"
+  },
+  {
+    "revision": "9e59ccd76c6f878b8482",
+    "url": "/tax-calculator/static/js/runtime-main.28dbfda8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taxCalculator/precache-manifest.c3fc1f5c897fe93767d42b2e06eb2579.js"
+  "/tax-calculator/precache-manifest.94433699626462502f239b0cea879464.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/taxCalculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tax-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
