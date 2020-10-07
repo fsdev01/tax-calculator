5d1cc6ab967b684307a7b8af463a97a238d8c86f
VH <vhuy371@gmail.com>
2020-10-08 10:54:15 +1100

    Updates

---
+++ precache-manifest.c3fc1f5c897fe93767d42b2e06eb2579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28272a954128256c26393b5423663f7",
+    "url": "/taxCalculator/index.html"
+  },
+  {
+    "revision": "de32a5c54b2a0f78d3b0",
+    "url": "/taxCalculator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a878905698bd854e7f65",
+    "url": "/taxCalculator/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "de32a5c54b2a0f78d3b0",
+    "url": "/taxCalculator/static/js/2.3fe3d4dd.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/taxCalculator/static/js/2.3fe3d4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a878905698bd854e7f65",
+    "url": "/taxCalculator/static/js/main.d76451e4.chunk.js"
+  },
+  {
+    "revision": "4ce67f44a006b0ce5ef8",
+    "url": "/taxCalculator/static/js/runtime-main.69ab1aac.js"
+  }
+]);
